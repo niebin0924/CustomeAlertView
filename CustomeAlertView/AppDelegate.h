8376fc87ad92b759c1cecbca9b6645be83745c4a
niebin0924 <804186194@qq.com>
2017-03-08 17:01:05 +0800

    Initial Commit

---
+++ CustomeAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomeAlertView
+//
+//  Created by Kitty on 17/3/8.
+//  Copyright © 2017年 Kitty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
